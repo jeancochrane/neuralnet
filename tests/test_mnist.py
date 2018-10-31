7b9ab1763105897aff567b1abdb6562b4ce77424
Jean Cochrane <jean@jeancochrane.com>
2018-10-31 17:07:28 -0400

    Load in MNIST data for testing

--- tests/test_mnist.py
+++ tests/test_mnist.py
@@ -3,10 +3,29 @@ import unittest
 import numpy as np
 
 import env  # Make sure that imports work
-import neuralnet.activation as activation
 
-from tensorflow.keras.datasets import mnist
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+class MNISTTestCase(unittest.TestCase):
 
-x_train.reshape(60000, 784)
+    @classmethod
+    def setUpClass(cls):
+        """
+        Import MNIST data.
+        """
+        # Function to turn batches of nx28x28 px training images into nx784 matrices.
+        reshape = lambda df: df.reshape((df.shape[0], 784))
+
+        # Load in the data.
+        with np.load('mnist.npz') as mnist:
+            x_train, x_test = reshape(mnist['x_train']), reshape(mnist['x_test'])
+            y_train, y_test = mnist['y_train'], mnist['y_test']
+
+        # Divide training data into training/validation sets.
+        train_idx = np.random.choice(range(x_train.shape[0]), int(0.7*x_train.shape[0]))
+        val_idx = np.array([i for i in range(x_train.shape[0]) if i not in train_idx])
+
+        cls.x_train, cls.y_train = x_train[train_idx], y_train[train_idx]
+        cls.x_val, cls.y_val = x_train[val_idx], y_train[val_idx]
+
+    def test_setup(self):
+        pass
